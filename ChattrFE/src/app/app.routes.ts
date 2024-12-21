10778c51b470372f445b0c12880808e688c9e01b
Alfred <alfredpoirier@videotron.ca>
2024-12-20 22:41:59 -0500

    added login page (not usable)

--- ChattrFE/src/app/app.routes.ts
+++ ChattrFE/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "/text",
+        pathMatch: "full"
+
+    },
+    {
+        path: "text",
+        // GUARD
+        loadComponent: () => import("../app/text/text.component")
+            .then(c => c.TextComponent)
+    },
+    {
+        path: "login",
+        loadComponent: () => import("../app/login-page/login-page.component")
+            .then(c => c.LoginPageComponent)
+    }
+];
