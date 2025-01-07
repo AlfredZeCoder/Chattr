4d7e16ce8cac713445b0a7031c0f5396fb209ecc
Valentin Adam <valentin.adam.job@gmail.com>
2025-01-07 16:40:01 -0500

    on va l<enlever

--- ChattrBE/src/user/user.service.ts
+++ ChattrBE/src/user/user.service.ts
@@ -7,8 +7,6 @@ import { AddUserDto } from 'src/dtos/add-user.dto';
 import { HashingServiceSingleton } from 'src/singletones/hashing.service.singleton';
 import { ConversationService } from 'src/conversation/conversation.service';
 import { ConversationServiceSingleton } from 'src/singletones/conversation.service.singleton';
-import { Conversation } from 'src/entities/conversation.entity';
-import { AddConversationDto } from 'src/dtos/add-conversation.dto';
 
 @Injectable()
 export class UserService implements OnModuleInit {
@@ -26,10 +24,6 @@ export class UserService implements OnModuleInit {
     }
 
     addUser = async (user: AddUserDto): Promise<User> => {
-        if (!user) {
-            throw new BadRequestException('User is required');
-        }
-
         if (!user.firstName) {
             throw new BadRequestException('First name is required');
         }
@@ -49,9 +43,9 @@ export class UserService implements OnModuleInit {
             throw new BadRequestException('User already exists');
         }
 
-        user.password = await this.hashingService.hashPassword(user.password);
-
-        const newUser = await this.userRepository.save(user);
+        let newUser = this.userRepository.create(user);
+        newUser.password = await this.hashingService.hashPassword(user.password);
+        newUser = await this.userRepository.save(newUser);
         return newUser;
     };
 
@@ -60,7 +54,7 @@ export class UserService implements OnModuleInit {
     };
 
     findOneById = async (id: number): Promise<User | null> => {
-        if (!id) {
+        if (!id || isNaN(id)) {
             throw new BadRequestException('User ID is required');
         }
         const user = await this.userRepository.findOneBy({ id });
@@ -75,15 +69,14 @@ export class UserService implements OnModuleInit {
             throw new BadRequestException('Email is required');
         }
         const user = await this.userRepository.findOneBy({ email });
-
-        if (!user) {
-            throw new BadRequestException('User not found');
-        }
-
         return user;
     };
 
     async addConversationToUser(userId: number, conversationId: number): Promise<void> {
+        if (!userId) {
+            throw new BadRequestException('User ID is required');
+        }
+
         if (!conversationId) {
             throw new BadRequestException('Conversation ID is required');
         }
@@ -92,26 +85,32 @@ export class UserService implements OnModuleInit {
         if (user.conversationsId.includes(conversationId)) {
             throw new BadRequestException('User already part of this conversation');
         }
-
         user.conversationsId.push(conversationId);
         await this.userRepository.save(user);
     }
 
 
     async deleteConversationById(userId: number, conversationId: number): Promise<void> {
+        if (!userId) {
+            throw new BadRequestException('User ID is required');
+        }
+
         if (!conversationId) {
             throw new BadRequestException('Conversation ID is required');
         }
 
         const user = await this.findOneById(userId);
 
-        if (!user.conversationsId.includes(conversationId)) {
+        if (!user.conversationsId.includes(+conversationId)) {
             throw new UnauthorizedException('User is not part of this conversation');
         }
 
-        user.conversationsId = user.conversationsId.filter(
-            (conversation) => conversation !== conversationId
-        );
+        for (let i = 0; i < user.conversationsId.length; i++) {
+            if (user.conversationsId[i] == conversationId) {
+                user.conversationsId.splice(i, 1);
+                break;
+            }
+        }
 
         await this.userRepository.save(user)
             .catch((error) => {
@@ -121,10 +120,15 @@ export class UserService implements OnModuleInit {
 
 
     async addPendingRequest(userId: number, askingUserId: number): Promise<void> {
-        if (userId == askingUserId) {
-            throw new BadRequestException('User cannot add himself as a friend');
+        if (userId == askingUserId){
+            throw new BadRequestException('Est tu si seul pour te parler a toi meme')
         }
-
+        if (!userId) {
+            throw new BadRequestException('User ID is required');
+        }
+        if (!askingUserId) {
+            throw new BadRequestException('User Request ID is required');
+        }
         const user = await this.findOneById(userId);
         await this.findOneById(askingUserId);
 
@@ -135,54 +139,38 @@ export class UserService implements OnModuleInit {
         }
         const conversations = await this.conversationService.getAllConversationsByUserId(userId);
         conversations.map((conversation) => {
-            if ((userId == conversation.createrUserId || userId == conversation.askedUserId) &&
-                (askingUserId == conversation.askedUserId || askingUserId == conversation.createrUserId)) {
-                throw new BadRequestException('A conversation already exist');
+            if (userId == conversation.createrUserId || userId == conversation.askedUserId &&
+                askingUserId == conversation.askedUserId || askingUserId == conversation.createrUserId){
+                throw new BadRequestException('A conversation already exist')
             }
-        });
+        })
         user.pendingUserIdRequests.push(askingUserId);
         await this.userRepository.save(user);
     }
 
 
-    async getAllPendingRequestsByUserId(userId: number) {
-        return (await this.findOneById(userId)).pendingUserIdRequests;
+    async getAllPendingRequestsByUserId(userId : number){
+        const user = await this.findOneById(userId);
+        return user.pendingUserIdRequests
     }
 
     async deletePendingRequest(userId: number, askingUserId: number): Promise<void> {
         const user = await this.findOneById(userId);
-        const previousLength = user.pendingUserIdRequests.length;
-
-        user.pendingUserIdRequests = user.pendingUserIdRequests.filter(
-            (pendingRequest) => pendingRequest !== askingUserId
-        );
-
-        if (previousLength === user.pendingUserIdRequests.length) {
-            throw new BadRequestException('User does not have a pending request from this user');
+        const userPendingRequests = await this.getAllPendingRequestsByUserId(userId)
+        const newPendingRequests = []
+        for (let index = 0; index < userPendingRequests.length; index++) {
+            const pendingRequest = userPendingRequests[index];
+            if (pendingRequest !== askingUserId){
+                newPendingRequests.push(pendingRequest);
+            }
         }
-
-        // const userPendingRequests = await this.getAllPendingRequestsByUserId(userId)
-        // const newPendingRequests = []
-        // for (let index = 0; index < userPendingRequests.length; index++) {
-        //     const pendingRequest = userPendingRequests[index];
-        //     if (pendingRequest !== askingUserId){
-        //         newPendingRequests.push(pendingRequest);
-        //     }
-        // }
-        // user.pendingUserIdRequests = newPendingRequests;
-        await this.userRepository.save(user);
+        user.pendingUserIdRequests = newPendingRequests;
+        await this.userRepository.save(user)
     }
 
-    async acceptPendingRequest(userId: number, askingUserId: number) {
-        if (!userId || !askingUserId) {
-            throw new BadRequestException('Both userId and askingUserId are required');
-        }
-        const conversationDto: AddConversationDto = {
-            createrUserId: askingUserId,
-            askedUserId: userId
-        };
-        await this.deletePendingRequest(userId, askingUserId);
-        const newConversation = this.conversationService.createConversation(conversationDto);
-        return newConversation;
+    async acceptPendingRequest() {
+        //create conversation
+        //delete pending request
+        //add conversation to user
     }
 }
